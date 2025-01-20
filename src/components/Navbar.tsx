92af47329da5538a726cbb4a2fe77dde267158bf
= <=>
2025-01-20 13:34:40 +0530

    add framer motion to templates page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import { faBarsStaggered, faClose } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { AnimatePresence, motion } from "framer-motion";
-import React, { useState } from "react";
+import  { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
@@ -16,7 +16,7 @@ const Navbar = () => {
             animate={{ opacity: 1, x: 0 }}
             exit={{ opacity: 0, x: 300 }}
             transition={{ duration: 0.5 }}
-            className="fixed right-0 z-50 h-screen w-80 p-3 font-serif bg-blue-900 bg-opacity-30 backdrop-blur-sm"
+            className="fixed right-0 z-[200] h-screen w-80 p-3 font-serif bg-blue-900 bg-opacity-30 backdrop-blur-sm"
           >
             <div className="flex justify-between mb-3 ">
               <NavLink to={"/"}><h3 className="text-2xl font-semibold text-blue-900">ResuMuse</h3></NavLink>
@@ -25,7 +25,7 @@ const Navbar = () => {
                 className="absolute top-4 right-4 text-white"
                 onClick={() => setIsVisible(false)}
               >
-                <FontAwesomeIcon icon={faClose} />
+                <FontAwesomeIcon icon={faClose} size="lg" />
               </button>
             </div>
             <div className="">
@@ -74,7 +74,7 @@ const Navbar = () => {
 
 
       {/* Navbar */}
-      <div className="sticky z-[200] backdrop-blur-lg  top-0 flex justify-between p-3 items-center shadow font-serif">
+      <div className="sticky z-[180] backdrop-blur-lg  top-0 flex justify-between p-3 items-center shadow font-serif">
         {/* Logo */}
         <div>
           <NavLink to={"/"}><h3 className="text-2xl font-semibold text-blue-900">ResuMuse</h3></NavLink>
@@ -126,7 +126,7 @@ const Navbar = () => {
 
 
           <button onClick={() => setIsVisible(!isVisible)}>
-            <FontAwesomeIcon icon={faBarsStaggered} />
+            <FontAwesomeIcon icon={faBarsStaggered} size="lg" className="text-blue-900"/>
           </button>
 
         </div>
