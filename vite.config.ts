8ab5d34adf45176b684edf707007f39556bc9782
= <=>
2025-02-11 12:06:05 +0530

    changed vite config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  server:{
-    port:3000
+  server: {
+    port: 3000,
+  },
+  build: {
+    minify: true,
+    target: "esnext",
+    sourcemap: false,
   },
   plugins: [react()],
-})
+});
